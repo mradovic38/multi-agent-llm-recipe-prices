950533b6c66e13c37215ab1ce2102d1e1f02fb96
mradovic38 <mihailo.radovic11@gmail.com>
2024-12-31 03:27:17 +0100

    Add contained and not conatined allergens attributes to product.py

--- scraping/product.py
+++ scraping/product.py
@@ -1,7 +1,7 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from decimal import Decimal
 
-from typing import Optional
+from typing import Optional, List
 
 @dataclass
 class Product:
@@ -11,3 +11,11 @@ class Product:
     unit: str
     package_size: str
     promo: Optional[float] = None
+    contains_allergens: Optional[List[str]] = field(default_factory=list)
+    does_not_contain_allergens: Optional[List[str]] = field(default_factory=list)
+
+    def __post_init__(self):
+        if self.contains_allergens is None:
+            self.contains_allergens = []
+        if self.does_not_contain_allergens is None:
+            self.does_not_contain_allergens = [] 
